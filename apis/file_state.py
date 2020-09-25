a0a968b29d97043818366bf378c2b355c665612d
Brock Palen <brockp@umich.edu>
2020-09-24 22:15:02 -0400

    fixed file_state api to use a post JSON document and not a query string

--- apis/file_state.py
+++ apis/file_state.py
@@ -1,8 +1,8 @@
 """API for eeadm file state."""
 import logging
 
-from flask import request, url_for
-from flask_restx import Namespace, Resource, fields, marshal
+from flask import request
+from flask_restx import Namespace, Resource, fields
 
 logging.getLogger(__name__).addHandler(logging.NullHandler)
 
@@ -13,24 +13,28 @@ api = Namespace(
 # model for returning data from eeadm file state -s
 # https://www.ibm.com/support/knowledgecenter/ST9MBR_1.3.0/ee_eeadm_file_state_command_output.html
 file_state_model = api.model(
-    "file_state", {"state": fields.String, "replicas": fields.Integer,}
+    "file_state", {"state": fields.String, "replicas": fields.Integer}
 )
-
 
 # model for the input of a file
 # must be abolute path
-file_parser = api.parser()
-file_parser.add_argument("path", type=str, required=True, help="absolute path to file")
+file_model = api.model("file", {"path": fields.String})
 
 
-# create teh API
+# create the API
 @api.route("/file_state")
 class FileState(Resource):
+    """API Provider class for eeadm file state.
+
+    https://www.ibm.com/support/knowledgecenter/ST9MBR_1.3.0/ee_eeadm_file_state_command_output.html
+    """
+
     @api.marshal_with(file_state_model)
-    @api.expect(file_parser, validate=True)
+    @api.expect(file_model, validate=True)
+    @api.response(404, "No such file")
     def post(self, **kwargs):
-        args = file_parser.parse_args()
-        path = args["path"]
+        """POST method to send payload of file path to check status of."""
+        path = request.json["path"]
 
         logging.debug(f"Checking state of {path} from {request.remote_addr}")
         return {"state": "M", "replicas": 2}
